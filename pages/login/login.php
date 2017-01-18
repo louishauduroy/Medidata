b7a68adee4ec62cfbb404889a61422b10afae7ca
Louis Hauduroy <louis.hauduroy@hotmail.fr>
2017-01-18 12:12:31 +0100

    login

--- pages/login/login.php
+++ pages/login/login.php
@@ -8,13 +8,41 @@
 
   <title>Medidata</title>
 
-  <script type="text/javascript" src="">
+  <link rel="stylesheet" href="../../jquery_bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="login.css">
+  <script src="../../jquery_bootstrap/jquery-3.1.1.min.js"></script>
+  <script src="login.js"></script>
 
   </script>
 
 </head>
 
 <body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <img class="logo" src="../../img/logo.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="wrap-login col-md-4 col-md-offset-4">
+        <form>
+          <h2 class ="titre_login">Login in Medidata</h2>
+          <div class="form-group">
+            <label for="">Email address</label>
+            <input type="email" class="form-control" id="" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="">Password</label>
+            <input type="password" class="form-control" id="" placeholder="Password">
+          </div>
+          <span class="login_message"></span>
+          <button type="submit" class="btn-login btn btn-default btn-lg btn-block">Submit</button>
+        </form>
+
+      </div>
+    </div>
+  </div>
 
 </body>
 
