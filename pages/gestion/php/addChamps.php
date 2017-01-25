5c6253e2b283bed954cbd079d653e2aec8287f3e
Louis Hauduroy <louis.hauduroy@hotmail.fr>
2017-01-25 01:20:02 +0100

    MAJ blindage gestion

--- pages/gestion/php/addChamps.php
+++ pages/gestion/php/addChamps.php
@@ -10,25 +10,62 @@ if(isset($_POST["champs"]) && isset($_POST["type"]) && $_POST["champs"]!="") {
   $resultat = $req->fetch();
 
   if(!$resultat){
-    $req2 = $bdd->prepare("INSERT INTO champs (id, name) VALUES ('$type', '$champs')");
-    $req2->execute();
-    $req2->closeCursor();
+		if( $type == 'DATE' ){
+			if (stripos($champs, 'date') !== FALSE){
+				if (stripos($champs, 'heure') !== FALSE) { echo 'Votre champs ne peut contenir le mot "heure"!'; }
+				else {
+					$req2 = $bdd->prepare("INSERT INTO champs (id, name) VALUES ('$type', '$champs')");
+			    $req2->execute();
+			    $req2->closeCursor();
 
-		if(stripos($type, 'INT') !== FALSE){
-			$requete = "ALTER TABLE certificat ADD $champs $type";
+					$requete = "ALTER TABLE certificat ADD $champs $type NOT NULL";
+
+			    $req3 = $bdd->prepare($requete);
+			    $req3->execute();
+			    $req3->closeCursor();
+					echo 'Champs ajouté !';
+				}
+			} else { echo 'Votre champs doit comporter le mot "date"!'; }
 		}
-		else {
-			$requete = "ALTER TABLE certificat ADD $champs $type NOT NULL";
+		else if ( $type == 'TIME' ){
+			if (stripos($champs, 'heure') !== FALSE){
+				if (stripos($champs, 'date') !== FALSE) { echo 'Votre champs ne peut contenir le mot "date"!'; }
+				else {
+					$req2 = $bdd->prepare("INSERT INTO champs (id, name) VALUES ('$type', '$champs')");
+			    $req2->execute();
+			    $req2->closeCursor();
+
+					$requete = "ALTER TABLE certificat ADD $champs $type NOT NULL";
+
+			    $req3 = $bdd->prepare($requete);
+			    $req3->execute();
+			    $req3->closeCursor();
+					echo 'Champs ajouté !';
+				}
+			} else { echo 'Votre champs doit comporter le mot "heure"!'; }
 		}
-    $req3 = $bdd->prepare($requete);
-    $req3->execute();
-    $req3->closeCursor();
-    echo 'Champs ajouté !';
+    else {
+			if (stripos($champs, 'date') !== FALSE){ echo 'Votre champs ne peut comporter le mot "date"!'; }
+			else if ( stripos($champs, 'heure') !== FALSE) { echo 'Votre champs ne peut comporter le mot "heure"!'; }
+			else {
+				$req2 = $bdd->prepare("INSERT INTO champs (id, name) VALUES ('$type', '$champs')");
+		    $req2->execute();
+		    $req2->closeCursor();
+
+				if(stripos($type, 'INT') !== FALSE){
+					$requete = "ALTER TABLE certificat ADD $champs $type";
+				}
+				else {
+					$requete = "ALTER TABLE certificat ADD $champs $type NOT NULL";
+				}
+		    $req3 = $bdd->prepare($requete);
+		    $req3->execute();
+		    $req3->closeCursor();
+		    echo 'Champs ajouté !';
+			}
+		}
   } else { echo 'Ce champs existe déja!'; }
-
   $req->closeCursor();
-}
-else { echo 'Entrez les info'; }
-
+} else { echo 'Entrez les info'; }
 
 ?>
